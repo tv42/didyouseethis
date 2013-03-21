7bdd75b04e91665c6202f0b70f3410f7a15c6ef8
Tommi Virtanen <tv@eagain.net>
2013-03-21 14:32:58 -0700

    Twitter sometimes refuses to retweet, with an obscure error.
    The conditions are poorly documented; this definitely triggers with
    non-protected accounts. Just handle it and move on.
    
    TODO: maybe move those work items aside.

--- cmd/didyouseethis-retweet/retweet.go
+++ cmd/didyouseethis-retweet/retweet.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/alloy-d/goauth"
+	"log"
 )
+
+type twitterError struct {
+	// https://code.google.com/p/twitter-api/issues/detail?id=1333
+	Errors string
+}
 
 func Retweet(id uint64, o *oauth.OAuth) error {
 	url := fmt.Sprintf(
@@ -19,7 +26,17 @@ func Retweet(id uint64, o *oauth.OAuth) error {
 		return err
 	}
 	if response.StatusCode != 200 {
-		return fmt.Errorf("twitter error: %s", response.Status)
+		var extra twitterError
+		if response.Header.Get("Content-Type") == "application/json; charset=utf-8" {
+			dec := json.NewDecoder(response.Body)
+			_ = dec.Decode(&extra)
+		}
+		if response.StatusCode == 403 && extra.Errors == "sharing is not permissible for this status (Share validations failed)" {
+			// this is not a temporary error, so stop trying
+			log.Printf("twitter refused retweet: %d", id)
+			return nil
+		}
+		return fmt.Errorf("twitter error: %s: %q", response.Status, extra.Errors)
 	}
 	return nil
 }
